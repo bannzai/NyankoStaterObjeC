bc32f4b5b5df3ad2c426708e7b7416f6650ce47b
hirose yudai <hirose@na-s.jp>
2015-08-06 14:12:53 +0900

    Initial Commit

---
+++ NyankoStarter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NyankoStarter
+//
+//  Created by hirose yudai on 2015/08/06.
+//  Copyright (c) 2015年 hirose yudai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NyankoStarter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NyankoStarter
+//
+//  Created by hirose yudai on 2015/08/06.
+//  Copyright (c) 2015年 hirose yudai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
