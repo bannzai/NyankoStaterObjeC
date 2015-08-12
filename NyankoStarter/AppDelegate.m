f2c8da52c5b472266bf5bf2b07ce190fcb48f78b
hirose yudai <hirose@na-s.jp>
2015-08-12 11:59:30 +0900

    Repro コード埋め込み

--- NyankoStarter/AppDelegate.m
+++ NyankoStarter/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Repro/Repro.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+//    [Repro setup:@""];
+//    [Repro startRecording];
     return YES;
 }
 
